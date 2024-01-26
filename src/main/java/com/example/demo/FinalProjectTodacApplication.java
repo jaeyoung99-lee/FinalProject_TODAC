48fe2274cc9a87ba7115d2c725b9dd63ae2460f0
jaeyoung <leejy0715@naver.com>
2024-01-26 16:09:52 +0900

    젠킨스 빌드 안되는 것을 프로젝트에서 git bash 열어서 해결

---
+++ src/main/java/com/example/demo/FinalProjectTodacApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectTodacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjectTodacApplication.class, args);
+	}
+
+}
---
+++ src/main/reactjs/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import mainImg from './image/하트.png';
+
+function App() {
+  return (
+    <div className="App">
+      <img alt='' src={mainImg} />
+      <h2><b>스프링부트 + 리액트 CI/CD 성공 기원!!!<br />한번에 성공하기</b></h2>
+      <h3>깃허브와 소스트리 연결 테스트</h3>
+    </div>
+  );
+}
+
+export default App;
