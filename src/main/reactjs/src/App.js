4882b91d6ddf23edc12dbc91056e3206da6cbc08
jaeyoung <leejy0715@naver.com>
2024-01-26 16:20:22 +0900

    [배포 오류 해결] -> [소스트리와 깃허브 재연결] 프로젝트를 감싸는 폴더를 깃허브에 연결하고 젠킨스에 빌드했더니 오류가 나서 프로젝트 폴더 자체를 깃허브에 연결해서 젠킨스에 빌드했더니 오류 해결 소스트리와 깃허브 재연결

--- src/main/reactjs/src/App.js
+++ src/main/reactjs/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <img alt='' src={mainImg} />
       <h2><b>스프링부트 + 리액트 CI/CD 성공 기원!!!<br />한번에 성공하기</b></h2>
-      <h3>깃허브와 소스트리 연결 테스트</h3>
+      <h3>깃허브와 소스트리 연결 테스트2</h3>
     </div>
   );
 }
